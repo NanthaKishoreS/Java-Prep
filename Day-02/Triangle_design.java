27eb6bee70c9be4876367e6475e491e9ecdd28db
Nantha Kishore S <Snanthakishore06@gmail.com>
2025-05-02 18:50:47 +0530

    Completed DAY 02

---
+++ Day-02/Triangle_design.java
@@ -0,0 +1,13 @@
+// Print Design Using Nested For Loop
+class Demo{
+	public static void main(String []args){
+		int c=1;
+		for(int i=0;i<6;i++){
+			for(int j=0;j<i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+}
