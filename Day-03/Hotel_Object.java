62ed1d6b38e1d3082be9424b477d693ff00c130b
Nantha Kishore S <Snanthakishore06@gmail.com>
2025-05-03 07:48:10 +0530

    Completed DAY 03

---
+++ Day-03/Hotel_Object.java
@@ -0,0 +1,9 @@
+public class Hotel {
+    int coffee = 20 ;
+    int tea = 10 ;
+    public static void main(String[]args){
+        Hotel server = new Hotel();
+        System.out.println(server.coffee);
+    }
+    
+}
