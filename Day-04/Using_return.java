301d1f426e6e8ed8d5f298698d637edcf2e2d830
Nantha Kishore S <Snanthakishore06@gmail.com>
2025-05-04 19:15:13 +0530

    Completed DAY 04

--- Day-04/Using_return.java
+++ Day-04/Using_return.java
@@ -11,3 +11,14 @@ class Demo{
 		System.out.println(rem);
 	}
 }
+//Get name using return
+class Demo{
+	String getname(String s1){
+		return s1;
+	}
+	public static void main(String []args){
+		Demo d = new Demo();
+		String name = d.getname("Nantha Kishore S");
+		System.out.println(name);
+	}
+}
