966c082dc61298480b401624ce07a10cd07408cc
Nantha Kishore S <Snanthakishore06@gmail.com>
2025-05-06 20:20:59 +0530

    Completed DAY 06

---
+++ Day-06/packages.java
@@ -0,0 +1,14 @@
+package package_one;
+
+public class Student {
+	public static void main(String[]args) {
+		Teacher t = new Teacher();
+		System.out.println(t.Salary);
+	}
+}
+package package_one;
+
+public class Teacher {
+	int Salary = 1000;
+
+}
