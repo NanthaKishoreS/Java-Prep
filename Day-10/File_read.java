535621d83354e1717b3f6049314d3dbdb04bc4c3
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-12 18:59:43 +0530

    Completed DAY 10

--- Day-10/File_read.java
+++ Day-10/File_read.java
@@ -15,3 +15,24 @@ public class MainClass01 {
 	}
 
 }
+
+package practice_java;
+import java.io.FileReader;
+public class MainClass01 {
+	public static void main(String[]args) {
+		try{
+			FileReader fr = new FileReader("Sample.txt");
+			int c = fr.read();
+			while(c !=-1) {
+				System.out.println((char)c);
+				c = fr.read();
+			}
+			fr.close();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+
+}
