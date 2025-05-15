ee9dda388d369723a3fc51032027b8a1b30b5703
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-15 08:46:00 +0530

    Completed DAY 11

---
+++ Day-11/Casting.java
@@ -0,0 +1,12 @@
+package nkfiles;
+
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+//        int a = scan.nextInt();
+        float a = scan.nextFloat();
+        System.out.println((int)a);
+    }
+}
