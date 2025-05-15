60d577212d557b9a8a950a986137879e0bf06e2b
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-15 09:43:34 +0530

    Completed DAY 11

--- Day-11/Type_Casting.java
+++ Day-11/Type_Casting.java
@@ -6,7 +6,10 @@ public class Typecasting {
     public static void main(String[] args) {
         Scanner scan =new Scanner(System.in);
 //        int a = scan.nextInt();
-        float a = scan.nextFloat();
-        System.out.println((int)a);
+        // float a = scan.nextFloat();
+        // System.out.println((int)a);
+
+        int number = 'a';
+        System.out.println(number);
     }
 }
