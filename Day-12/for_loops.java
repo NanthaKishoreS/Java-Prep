8dee4f5fcb02a50938fb2809edf6c54779054ec4
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-16 10:54:07 +0530

    Completed DAY 12

--- Day-12/for_loops.java
+++ Day-12/for_loops.java
@@ -1,12 +1,19 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Loops {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // Print from i to n
         int n = scan.nextInt();
-        for(int i=1;i<=n;i++) {
-            System.out.println(i);
+//        for(int i=1;i<=n;i++) {
+//            System.out.println(i);
+//        }
+        int num = 1;
+        while (num <= n) {
+            System.out.println(num);
+            num++;
         }
     }
 }
+ 
