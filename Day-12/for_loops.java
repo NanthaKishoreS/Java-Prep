407acd3affce68e8d3ed9ea6b2ccd6b7a8640e22
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-16 10:50:40 +0530

    Completed DAY 12

---
+++ Day-12/for_loops.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // Print from i to n
+        int n = scan.nextInt();
+        for(int i=1;i<=n;i++) {
+            System.out.println(i);
+        }
+    }
+}
