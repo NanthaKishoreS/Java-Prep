07039070e9f04621d32724129e092db06377a259
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-16 07:36:09 +0530

    Completed DAY 12

---
+++ Day-12/loop.java
@@ -0,0 +1,21 @@
+package nkfiles;
+
+public class Basic {
+    public static void main(String[] args) {
+//        if(5<4){
+//            System.out.println("Hello World ");
+//        }
+//        else{
+//            System.out.println("World Hello");
+//        }
+//        int count = 1;
+//        while(count != 5){
+//            System.out.println("Hello World ");
+//            count++;
+//        }
+//        for(int count = 0;count!=5;count++){
+//            System.out.println("Hello World !!!");
+//        }
+        
+    }
+}
