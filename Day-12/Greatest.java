cceab691b3c4fb80b7b082472fcfcc2cfe87ebfc
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-16 11:06:44 +0530

    Completed DAY 12

---
+++ Day-12/Greatest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        if(a>b){
+            if(a>c){
+                System.out.println(a + "Is the Greatest Number ");
+            }
+            else{
+                System.out.println(c +"  Is the Greatest Number ");
+            }
+        }
+        else{
+            if(b>c){
+                System.out.println(b+ " Is the Greatest Number ");
+            }
+            else{
+                System.out.println(c +"  Is the Greatest Number ");
+            }
+        }
+    }
+}
