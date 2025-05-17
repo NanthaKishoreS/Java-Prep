d6c087fd68651c727ecb2b1c21d4f1954151b369
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-17 18:06:19 +0530

    Completed DAY 13

---
+++ Day-13/Occurrence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Occurrence {
+    public static void main(String[] args) {
+        //Count the Occurrence of a number in the number
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter The Number : ");
+        int num = scan.nextInt();
+        System.out.print("Enter the Number Which You want to Count the Occurrence : ");
+        int numc = scan.nextInt();
+        int count  = 0;
+        while(num>0){
+            if(numc==(num%10)){
+                count++;
+            }
+            num = num/10;
+        }
+        System.out.println(count);
+
+
+    }
+}
