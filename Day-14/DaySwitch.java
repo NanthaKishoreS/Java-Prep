e659ddd43dd85eab1b6f7febafa3f0d70cfa724f
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-18 17:59:16 +0530

    Completed DAY 14

---
+++ Day-14/DaySwitch.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter A Number To print the Day of the Week");
+        int day = scan.nextInt();
+        switch(day){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Enter Number From 1 to 7 Only ");
+        }
+
+    }
+}
+/*Ouput
+Enter A Number To print the Day of the Week
+2
+Tuesday
+
+Process finished with exit code 0
+*/ 
