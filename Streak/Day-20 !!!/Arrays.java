8972c5c67e5276191a759368c768b38785ef0ee2
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-24 20:46:31 +0530

    DAY 20 Completed

--- Streak/Day-20 !!!/Arrays.java
+++ Streak/Day-20 !!!/Arrays.java
@@ -4,9 +4,12 @@ public class Code {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] arr = new int[5];
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i <arr.length; i++){
             arr[i]=scan.nextInt();
         }
-        System.out.println(arr[0]);
+        for (int i = 0; i <arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
     }
 }
