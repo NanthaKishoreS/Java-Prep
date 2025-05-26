ecb3cb914216b960900f1cb27b30f7753844d218
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-26 13:14:30 +0530

    DAY 22 Completed

---
+++ Streak/Day-22 !!!/2D_Search.java
@@ -0,0 +1,28 @@
+package Search;
+import java.util.*;
+public class Two_DimSearch {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3,4,5},
+                {6,7,8,9,0}
+        };
+        int target = 0;
+        int[] result  = Twodim(arr,target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] Twodim(int[][] arr, int target) {
+        if(arr.length==0){
+            return new int[]{-1,-1};
+        }
+        for(int i = 0;i<arr.length;i++){
+            for (int j = 0; j < arr[i].length; j++) {
+                if(target==arr[i][j]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+
+    }
+}
