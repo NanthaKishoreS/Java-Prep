c789cf01f55457d0decb2487a067e3873af0e40f
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-26 10:53:22 +0530

    DAY 22 Completed

---
+++ Streak/Day-22 !!!/Linear_Search.java
@@ -0,0 +1,26 @@
+package Search;
+import java.util.*;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.print("Enter The Number Of Search in the Array : ");
+        int num = scan.nextInt();
+        int result = Linear(arr,num);
+        if(result!=-1){
+            System.out.println("The Element is present in the index : "+result);
+        }
+        else
+            System.out.println("Element Not found !!");
+
+    }
+    static int Linear(int a[],int n){
+        for(int i = 0;i<a.length;i++){
+            if (a[i] == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
