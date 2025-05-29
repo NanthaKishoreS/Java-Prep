72a609406da1ee99ff0fc3469a5238aec56920d9
Nantha Kishore S <nanthakishore06@gmail.com>
2025-05-29 20:23:12 +0530

    DAY 25 Completed

--- Streak/Day-25 !!!/Ceiling.java
+++ Streak/Day-25 !!!/Ceiling.java
@@ -1,5 +1,25 @@
-class Ceiling{
-  public static void main(String[] args){
-    System.out.println("Hello World");
-  }
+package Binary;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {1,23,34,45,56,67,78,89,90};
+        int ans = ceil(arr,68);
+        System.out.println(ans);
+
+    }
+    static int ceil(int[] arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+            while(start<=end) {
+            int mid = (start+(end-start)/2);
+            if(arr[mid]>target){
+                end = mid -1;
+            }
+            else if (arr[mid]<target){
+                    start = mid+1;
+            }else return mid;
+
+        }
+        return start;
+    }
 }
