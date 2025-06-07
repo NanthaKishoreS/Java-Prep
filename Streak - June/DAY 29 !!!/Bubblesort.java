c266053194d289ba99478dd20b5bc8f3fb4a4e20
Nantha Kishore S <nanthakishore06@gmail.com>
2025-06-07 15:13:23 +0530

    DAY 29 Completed

---
+++ Streak - June/DAY 29 !!!/Bubblesort.java
@@ -0,0 +1,20 @@
+package Bubble;
+import java.util.Arrays;
+public class Sort {
+    public static void main(String[] args) {
+        int[] arr = {1,5,7,3,7,31,6,8,9,5,3};
+        sorted(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sorted(int[] arr){
+        for(int i =0;i<arr.length;i++){
+            for (int j = 1; j < arr.length-i-1; j++) {
+                if(arr[j]>arr[j+1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
